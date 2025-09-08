95ff7a5a8aea373387a37ecb3ae966596031c85d
ellieCorner <ellie25corner@gmail.com>
2025-09-07 22:37:33 -0700

    refactor: reactor About section

---
+++ final-project/src/components/About/data.ts
@@ -0,0 +1,18 @@
+export const ABOUT_ITEMS = [
+  {
+    title: "Our Mission",
+    text: "At Ddsgnr Auto Repair, our mission is to provide our customers with the highest level of car care and customer service.",
+  },
+  {
+    title: "Our Expertise",
+    text: "At Ddsgnr Auto Repair, our technicians are the heart and soul of our business. With years of experience and training.",
+  },
+  {
+    title: "Our Equipment",
+    text: "At Ddsgnr Auto Repair, we invest in state-of-the-art tools and equipment to ensure precision and reliability in every repair.",
+  },
+  {
+    title: "Our Customer Promise",
+    text: "At Ddsgnr Auto Repair, we are committed to delivering transparent, honest, and dependable service to every customer.",
+  },
+];
